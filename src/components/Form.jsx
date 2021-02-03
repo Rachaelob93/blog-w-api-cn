b424f8dbc33a2e96c5270fdbb45784899f6ab340
Rachael O'Brien <rachaelob93@gmail.com>
2021-02-03 17:32:15 +0000

    attempt to post a post to database - still broken

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -17,22 +17,59 @@ import React from "react";
 // }
 
 const PostForm = () => {
+
+    const getAuthors = async (e) => { 
+        e.preventDefault();
+        let dataGlobal =  await fetch(`http://localhost:5000/users`).then(response => response.json())
+        console.log(dataGlobal);
+        for(let i = 0; i < dataGlobal.length; i++){
+            console.log(dataGlobal[i]._id)
+            let option = document.createElement("option");
+            option.text= dataGlobal[i].name;
+            option.value = dataGlobal[i]._id;
+            let select = document.getElementById("author");
+            select.appendChild(option);
+        }
+     let authorButton = document.getElementById("author-button")
+     authorButton.remove();
+    };
         const fetchData = async (e) => { 
         e.preventDefault();
         console.log(e.target[0].value)
         console.log(e.target[1].value)
         console.log(e.target[2].value)
-        await fetch(`http://localhost:5000/posts`).then(response => response.json()).then(data => console.log(data[0].content))
+        const myObject = {
+            title: e.target[0].value,
+            content: e.target[1].value,
+            author: e.target[2].value
+        }
+        console.log(myObject)
+        console.log(JSON.stringify(myObject))
+        try {
+            await fetch("http://localhost:5000/posts/601ad57572f0de2ca0164787", //<--------------- HARD CODED _ID NEEDS FIXING STILL BROKED
+            {
+                method: 'POST',
+                headers: 'application/json',
+                body: JSON.stringify(myObject)
+    
+            }).then(response => response.json()).then(data => console.log(data[0].content))
+        } catch (error) {
+            console.log(error)
+        }
+       
         // const data = await response;
         // console.log(data);
     };
     return(
         <div>
+            <button id="author-button" onClick={getAuthors}>Get Authors</button>
             <form action="post" onSubmit={fetchData}>
                 <label>Post </label>
                 <input type="text" id="title" placeholder="Title"/>
                 <input type="text" id="content" placeholder="Content"/>
-                <input type="text" id="author" placeholder="Author"/>
+                <select name="author" id="author">
+                 
+                </select>
                 <input type="submit" name="submit"/>
             </form>
         </div>
