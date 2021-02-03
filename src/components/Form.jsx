10b5048daf608cb04a3f2fd04298ed6c57af1147
Rachael O'Brien <rachaelob93@gmail.com>
2021-02-03 15:53:05 +0000

    log values from inputs under PostForm

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 
+
+
 // const search = () => {
 //     alert("I got clicked!");
 // };
@@ -15,9 +17,18 @@ import React from "react";
 // }
 
 const PostForm = () => {
+        const fetchData = async (e) => { 
+        e.preventDefault();
+        console.log(e.target[0].value)
+        console.log(e.target[1].value)
+        console.log(e.target[2].value)
+        await fetch(`http://localhost:5000/posts`).then(response => response.json()).then(data => console.log(data[0].content))
+        // const data = await response;
+        // console.log(data);
+    };
     return(
         <div>
-            <form action="post">
+            <form action="post" onSubmit={fetchData}>
                 <label>Post </label>
                 <input type="text" id="title" placeholder="Title"/>
                 <input type="text" id="content" placeholder="Content"/>
@@ -31,19 +42,20 @@ const PostForm = () => {
 
 
 const SearchUsers = () => {
-    const fetchData = async () => { 
-    const response = await fetch(`http://localhost:5000/users`).then(response => response.json()).then(data => console.log(data))
+    const fetchData = async (e) => { 
+    e.preventDefault();
+    await fetch(`http://localhost:5000/users`).then(response => response.json()).then(data => console.log(data))
     // const data = await response;
     // console.log(data);
+ 
 };
+
     return(
         <div>
-            <form action="post" onSubmit={fetchData()}>
+            <form action="post" onSubmit={fetchData} >
                 <label>Search </label>
                 <input type="text" id="search-title" placeholder="Title"/>
-                <input type="text" id="content" placeholder="Content"/>
-                <input type="text" id="author" placeholder="Author"/>
-                <input type="submit" name="submit"/>
+                <input type="submit" name="submit" />
             </form>
         </div>
     );
